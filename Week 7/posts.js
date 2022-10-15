dd67acf086d4ce51dae4790c09b880be133615fe
Cosmin Vasile <cosmin.vasile@scoalainformala.ro>
2022-10-15 13:55:19 +0300

    Add Week 7

---
+++ Week 7/posts.js
@@ -0,0 +1,72 @@
+function runWeatherApp() {
+  var weatherKey = "3e20f53fb2ef40e28bf95905221510";
+  var weatherBaseUrl = "https://api.weatherapi.com/v1";
+
+  var temperatureButtonEl = document.querySelector("#temperature-button");
+  temperatureButtonEl.addEventListener("click", onTemperatureButtonClick);
+  var temperatureContainerEl = document.querySelector("#temperature-container");
+
+  function onTemperatureButtonClick() {
+    var queryParams = "key=" + weatherKey + "&q=Craiova";
+    fetch(weatherBaseUrl + "/current.json?" + queryParams)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (weather) {
+        var myWeather = weather || {};
+        var currentWeather = myWeather.current || {};
+        var temperature = currentWeather.temp_c || null;
+        if (temperature !== null) {
+          temperatureContainerEl.style = null;
+          var temperatureEl =
+            temperatureContainerEl.querySelector("#temperature");
+          temperatureEl.innerHTML = temperature;
+        }
+      });
+  }
+}
+
+function runPostsApp() {
+  var postsBaseUrl = "https://jsonplaceholder.typicode.com";
+  var postsContainerEl = document.querySelector("#posts-container");
+
+  function renderPostList(posts) {
+    posts.forEach(function (post) {
+      var postDiv = createPost(post.title, post.body);
+      if (postDiv) {
+        postsContainerEl.appendChild(postDiv);
+      }
+    });
+  }
+
+  function createPost(title, body) {
+    var div = document.createElement("div");
+    div.innerHTML =
+      "<div class='post-title'>" +
+      "<h2>" +
+      title +
+      "</h2>" +
+      "</div><p class='post-body'>" +
+      body +
+      "</p>";
+
+    return div;
+  }
+
+  fetch(postsBaseUrl + "/posts")
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (posts) {
+      renderPostList(posts);
+    });
+}
+
+var pageInit = function () {
+  runWeatherApp();
+  runPostsApp();
+};
+
+window.addEventListener("load", function () {
+  pageInit();
+});
